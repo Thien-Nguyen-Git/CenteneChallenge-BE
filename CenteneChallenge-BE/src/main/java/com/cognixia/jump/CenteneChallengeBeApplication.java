544f09caf24dffbc21e84a06a3c2e6ae63588e3a
Thien Nguyen <65026941+Thien-Nguyen-Git@users.noreply.github.com>
2020-10-18 18:04:07 -0400

    Spring Boot

---
+++ CenteneChallenge-BE/src/main/java/com/cognixia/jump/CenteneChallengeBeApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenteneChallengeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenteneChallengeBeApplication.class, args);
+	}
+
+}
